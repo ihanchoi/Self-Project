bb3d81a2bcce206900dbb8e04f9cbe55a63dccac
ihanchoi <ihanchoi.dev@gmail.com>
2023-07-18 23:26:27 +1000

    SETTING

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="Timebridge">
+      <div>
+      <h3> Username location</h3>
+      <select>
+        <h2 className='label-wrapper'>
+          <label htmlFor='location-info' className='loca_info'></label>
+        </h2>
+      </select>
+      </div>
+      <div>
+      <h3> Username location</h3>
+      <select>
+        <h2 className='label-wrapper'>
+          <label htmlFor='location-info' className='loca_info'></label>
+        </h2>
+      </select>
+      </div>
+    </div>
+  );
+}
+
+export default App;
